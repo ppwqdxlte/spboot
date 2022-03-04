ce955682ea66ebf218aa00035320d9e769392445
ppwqdxlte <rexinwangmin@foxmail.com>
2022-03-04 12:52:31 +0800

    将Hikari改成Druid，配置连接池，并测试连接性，配置druid监控，添加web依赖，访问http://localhost:8080/druid/index.html 即进入德鲁伊的监控页面

--- class03/src/main/java/com/laowang/springbootdata/Class03Application.java
+++ class03/src/main/java/com/laowang/springbootdata/Class03Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Class03Application {
-
+    /*
+    * 启动项目，http://localhost:8080/druid/index.html 德鲁伊监控页面，管理员和密码见application.yaml中的配置
+    * */
     public static void main(String[] args) {
         SpringApplication.run(Class03Application.class, args);
     }
--- class03/src/test/java/com/laowang/springbootdata/Class03ApplicationTests.java
+++ class03/src/test/java/com/laowang/springbootdata/Class03ApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -17,11 +16,11 @@ class Class03ApplicationTests {
     private DataSource dataSource;
 
     @Test
-    void singleConnection(){ //可知spring boot默认HikariPool连接池
+    void singleConnection(){ //可知spring boot默认HikariPool连接池,但是改成德鲁伊后就是德鲁伊的数据源了
         Connection connection = null;
         //HikariProxyConnection@1426040161 wrapping com.mysql.cj.jdbc.ConnectionImpl@49c099b
         //HikariProxyConnection包装的mysql..ConnectionImpl的实例
-//        System.out.println(dataSource.getClass().getName());
+        System.out.println(dataSource.getClass().getName());
         try {
             System.out.println(Thread.currentThread().getName()+"......."+(connection = dataSource.getConnection()));
         } catch (SQLException e) {
@@ -39,6 +38,7 @@ class Class03ApplicationTests {
 
     @Test
     void mainThreadGetSeveralConnections(){ //main线程获得多个connection，可能每个都对应不同的Hikari线程池的连接
+        //druid就不同了！【默认情况】同个thread不管获取多少次connection，都会获得相同的druid连接！！
         for (int i = 0; i < 100; i++) {
             Connection connection = null;
             try {
@@ -61,6 +61,8 @@ class Class03ApplicationTests {
     void eachThreadGetsItsOwnConnection(){ //每个线程都有各自的Hikari连接池连接，
         // 多次运行测试方法可知，连接对象会出现很多次，
         // 说明Hikari线程池早已被spring容器创建，并且提前准备好若干连接对象以备随时取用
+        //【默认情况连接池只有1个连接对象】多线程同时运行，也只获得同一个druid连接，除非在application.yaml中改变配置！
+        //https://blog.csdn.net/m0_37989980/article/details/105587908
         for (int i = 0; i < 10; i++) {
             new Thread(()->{
                 singleConnection();
